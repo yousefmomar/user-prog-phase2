5b1c0c826e0b58f3e958b3ab7c2e0fe4cd2dbcba
youssef-omarrr <es-youssef.moh.ah2026@alexu.edu.eg>
2025-05-09 22:35:24 +0300

    wait done

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -131,6 +131,10 @@ struct thread
     struct child_process *cp;
 
     struct file *executable; // used to deny writes to executable file
+
+
+    struct list child_list;          /* List of children */
+    struct child_status *wait_status;/* Status shared with parent */
   };
 
 /* If false (default), use round-robin scheduler.
